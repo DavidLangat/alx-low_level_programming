6f16954d5c499f1385e296870eef6b8107b3c758
DavidLangat <dkipkirui12@gmail.com>
2022-09-08 12:47:59 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+
 /**
-*main
+*main - Main function of my project
 *
 *
 *Return: return 0 everytime
@@ -9,6 +10,7 @@
 int main(void)
 {
     char displayed [52];
+    
     strcpy(displayed,"\"Programming is like building a multilingual puzzle");
     puts(displayed); 
     return (0);
