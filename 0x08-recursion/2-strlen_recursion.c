d5b78e036e8b875a2ba47214023dc7055cc4194e
David <93221294+DavidLangat@users.noreply.github.com>
2022-10-29 09:35:46 +0300

    0x08. C - Recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,18 @@
 #include "main.h"
-
 /**
- * _strlen_recursion - size
- * @s: pointer to string params
- * Return: recursion
- */
+* _strlen_recursion - gives length of a string
+* @s: string to be measured
+*Return: length of the string
+*/
 
 int _strlen_recursion(char *s)
 {
-	if (!*s)
+	int len = 0;
+
+	if (*s != '\0')
 	{
-		return (0);
+		len++;
+		len += _strlen_recursion(s + 1);
 	}
-	return (1 + _strlen_recursion(++s));
+	return (len);
 }
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- * factorial - factorial n
- * @n: integer arams
- * Return: recursion
- */
+* factorial - searchs for the factorial of a number
+* @n: the number to find the factorial of
+*
+* Return: the factorial of the number
+*/
 
 int factorial(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	if (n == 1)
-	{
+	int next;
+
+	if (n == 0)
 		return (1);
-	}
-	return (n * factorial(n - 1));
+	else if (n < 0)
+	return (-1);
+
+	next = factorial(n - 1);
+	return (n * next);
 }
+
